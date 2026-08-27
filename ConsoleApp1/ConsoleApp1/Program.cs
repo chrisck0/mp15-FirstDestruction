526dc7a2305e60f9d06a97b4f0c5a56e64bcf85c
chrisck0 <chriscklucky@gmail.com>
2026-08-27 15:15:07 +0900

    [Feat] 콘솔 추가 출력 제거

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,5 @@ public class Program
     static void Main(string[] args)
     {
         Monster monster = new Monster(20, 10);
-        Console.WriteLine($"Monster 체력 : {monster.Health} 공격력 : {monster.Attack}");
     }
 }
