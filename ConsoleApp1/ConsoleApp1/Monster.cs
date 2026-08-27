4c68daf874eae14779959b25ba40852bd02f110c
chrisck0 <chriscklucky@gmail.com>
2026-08-27 14:30:52 +0900

    Monster 클래스 생성자 추가

--- ConsoleApp1/ConsoleApp1/Monster.cs
+++ ConsoleApp1/ConsoleApp1/Monster.cs
@@ -2,6 +2,13 @@
 
 public class Monster
 {
-    public int Health { get; set; }
+    public int Health { get; private set; }
+    public int Attack { get; private set; }
+
+    public Monster(int health, int attack)
+    {
+        Health = health;
+        Attack = attack;
+    }
 }
 
